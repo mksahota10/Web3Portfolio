71643935749616e95c8a09d3b09b94843eed763e
mksahota10 <mksahota10@gmail.com>
2022-06-06 16:10:13 -0700

    splineworking, navbar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import Spline from '@splinetool/react-spline';
 
 function App() {
   return (
-    <div className="flex w-screen h-screen items-center justify-center">
-      <p>Hello!</p>
-     
+    <div className="w-screen h-screen flex-col items-center justify-center relative bg-primary pb-20">
+
+      {/*Navigation Bar */}
+      <nav className="w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center">
+        <div className="w-full md:w-880 bg-navBar p-4 rounded-2x1 flex items-center">
+          <p className="text-lg text-slate-200 font-medium">Manpreet Kaur</p>
+        </div>
+      </nav>
+
+
+
+
+     <div className="relative" id="home">
+       <Spline scene="https://draft.spline.design/1SIsKQFH-TVtEI-0/scene.spline" />
+       
+     </div>
     </div>
   );
 }
